43933ba5a6a6abc9f6f6a47dcaee41ce062e1ade
Danielle <goncalvesdani54@hotmail.com>
2023-11-23 16:05:46 -0300

    finalizando projeto v1

--- assessment_disc_project/settings.py
+++ assessment_disc_project/settings.py
@@ -108,6 +108,7 @@ AUTHENTICATION_BACKENDS = [
 #AUTH_USER_MODEL = 'user_auth.CustomUser'  # Substitua 'user_auth' pelo nome do seu app onde o CustomUser foi definido
 AUTH_USER_MODEL = 'user_auth.CustomUser' #modo para colocar o allauth
 LOGIN_REDIRECT_URL = 'user_auth:perfil'  # Ajuste o namespace e o nome da view conforme o definido no seu urls.py
+ACCOUNT_SIGNUP_REDIRECT_URL = '/disc/teste_disc/' #para onde o usuario vai ser redirecionado depois de se cadastrar
 ACCOUNT_EMAIL_VERIFICATION = 'none' #para não precisar confirmar o email
 
  #para onde o usuario vai ser redirecionado depois de logar
